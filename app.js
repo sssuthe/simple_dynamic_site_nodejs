ebac555a34337d6465b86de5df87820cfb293acc
Sean Sutherland <sean.sutherland@mc02gt2undv16.readytalk.com>
2016-04-25 11:11:58 -0600

    completed state of creating a simple server

--- app.js
+++ app.js
@@ -5,7 +5,10 @@
 var http = require('http');
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
+  setInterval(function(){
+  response.write(new Date() + "\n");
+  }, 1000);
+  //response.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
