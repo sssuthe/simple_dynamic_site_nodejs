5db5a68a340187ef0821af0d000cf465abb8d91d
Sean Sutherland <sean.sutherland@mc02gt2undv16.readytalk.com>
2016-05-13 09:57:57 -0600

    site is working and redirecting based on get

--- renderer.js
+++ renderer.js
@@ -3,11 +3,21 @@
        //merge values into string
 var fs = require('fs');
 
+function mergeValues(values, content){
+    //cycle over the keys
+    for(var key in values){
+        //Replace all the {{key}} with the value from the values object
+        content = content.replace("{{" + key + "}}", values[key])
+    }
+    //return merged content
+    return content;
+}
+
   function view(templateName, values, response){
     //Read from the template file
-    var fileContents = fs.readFileSync('./views/' + templateName + '.html');
+    var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
       //Insert values in to the content
-
+      fileContents = mergeValues(values, fileContents);
       //Write out the contents to the response
       response.write(fileContents);
   }
--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 var Profile = require("./profile.js");
-var renderer = require("./renderer.js")
-
-
+var renderer = require("./renderer.js");
+var querystring = require("querystring");
+var commonHeaders = {'Content-Type': 'text/html'};
 
 
 // We want to handle the http route GET / and POST / i.e. Home
@@ -9,25 +9,36 @@ function home(request, response)
 {
     //if url == "/" && GET
     if (request.url === "/"){
-    //show search
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    renderer.view("header", {}, response);
-    renderer.view("search", {}, response);
-    renderer.view("footer", {}, response);
-    response.end();
+        if (request.method.toLowerCase() === "get") {
+            //show search
+            response.writeHead(200, commonHeaders);
+            renderer.view("header", {}, response);
+            renderer.view("search", {}, response);
+            renderer.view("footer", {}, response);
+            response.end();
+        } else {
+                 //if url == "/" && POST
+                 //get the post data from body
+                 request.on("data", function(postBody){
+                      var query = querystring.parse(postBody.toString());
+                      response.writeHead(303, {"Location": "/" + query.username})
+                      response.end();
+                 });
+                 //extract the username
+                 //redirect to /:username
+        }
     }
 }
 
         //show search
-     //if url == "/" && POST
-        //redirect to /:username
+
 
 // Handle HTTP route GET /:username i.e. /seansutherland
 function user(request, response){
      //if url == "/...."
      var username = request.url.replace("/", "");
      if (username.length > 0){
-         response.writeHead(200, {'Content-Type': 'text/plain'});
+         response.writeHead(200, commonHeaders);
          renderer.view("header", {}, response);
 
          //Get json from Treehouse
