8637a79f0595a1d6a8a8a26f777f10e5862792a6
Sean Sutherland <sean.sutherland@mc02gt2undv16.readytalk.com>
2016-05-13 14:09:55 -0600

    removed IP from app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ http.createServer(function (request, response) {
      router.home(request, response);
      router.user(request, response);
 
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(1337);
+console.log('Server running locally on port 1337/');
 
 
 
